91e6338764912fb2676206cc86fcfc62f62f32f9
Himanshu.01 <21ucs095@lnmiit.ac.in>
2026-01-05 00:17:05 +0530

    "Service config file added to store registered data"

---
+++ src/main/java/com/ratelimiter/requestDto/ServiceConfig.java
@@ -0,0 +1,40 @@
+package com.ratelimiter.requestDto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServiceConfig {
+
+    private String name;
+
+    private String pathPrefix;
+
+    private String targetUrl;
+
+    private boolean stripPrefix;
+
+    public boolean matches(String requestPath) {
+        if (requestPath == null || pathPrefix == null) {
+            return false;
+        }
+        return requestPath.startsWith(pathPrefix);
+    }
+
+    public String transformPath(String requestPath) {
+        if (stripPrefix && requestPath.startsWith(pathPrefix)) {
+            String transformed = requestPath.substring(pathPrefix.length());
+            if (!transformed.startsWith("/")) {
+                transformed = "/" + transformed;
+            }
+            return transformed;
+        }
+        return requestPath;
+    }
+}
